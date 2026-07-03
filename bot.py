c4a80ed811150b24a6113fb0c073b2113530fb19
e9pip-01 <e9pip.01@gmail.com>
2026-07-03 02:55:44 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -259,6 +259,15 @@ async def process_youtube_search(message: Message, text: str):
         'socket_timeout': 15,
         'http_chunk_size': 1048576,
         'external_downloader': 'curl_cffi',
+        'external_downloader_args': ['--impersonate', 'chrome'],
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Sec-Ch-Ua': '"Google Chrome";v="144", "Not=A?Brand";v="8", "Chromium";v="144"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Windows"',
+        },
         'progress_hooks': [progress_hook],
     }
     
